f0d3d1cec2a15b89a75869a894fc878a0d590c74
rosefa <82177268+rosefa@users.noreply.github.com>
2022-01-31 14:21:15 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,6 @@
 #SBATCH -N 1                  # 1 nœud
 #SBATCH --exclusive           # Le nœud sera entièrement dédié à notre job, pas de partage de ressources
 #SBATCH -t 05:00:00           # Le job sera tué au bout de 5h
-#SBATCH --gres=gpu:4          # 4 GPU par nœud
 #SBATCH --constraint=gpudp    # Nœuds GPU double précision
 #SBATCH --mail-type=END       # Réception d'un mail à la fin du job
 #SBATCH --mail-user=tokparosali@gmail.com
