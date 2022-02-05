f19388222232208dbb02e7defc8fcadbc25efae6
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-05 01:52:20 +0100

    Update DCNN-DANN.py

--- DCNN-DANN.py
+++ DCNN-DANN.py
@@ -187,15 +187,18 @@ def build_bilstm(word_index, embeddings_dict, optimizer='adam', MAX_SEQUENCE_LEN
 
     model1=Conv1D(128, 5, activation="relu")(embedding_layer)
     model1=Conv1D(128, 5, activation="relu")(model1)
+     model1= MaxPooling1D(2)(model1)
     model1= Conv1D(256, 5, activation="relu")(model1)
     model1 = BatchNormalization()(model1)
     model1= MaxPooling1D(2)(model1)
     model1= Dense(256,activation='relu')(model1)
   
-    model2 = Dense(units=128, kernel_initializer='glorot_uniform', activation='relu')(embedding_layer)
-    model2 = Dense(units=64, kernel_initializer='glorot_uniform', activation='relu')(model2)
+    model2=Conv1D(128, 5, activation="relu")(embedding_layer)
+    model2= MaxPooling1D(2)(model2)
+    model2 = Dense(units=128, activation='relu')(model2)
+    model2 = Dense(units=64, activation='relu')(model2)
     model2 = Dropout(rate=0.5)(model2)
-    model2 = Dense(units=64, kernel_initializer='glorot_uniform', activation='relu')(model2)
+    model2 = Dense(units=64, activation='relu')(model2)
     model2= Dense(256,activation='relu')(model2)
     
     #model2 = Dropout(rate=0.5)(model2)
