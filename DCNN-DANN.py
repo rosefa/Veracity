6083d3e7c4d493a136291726e463c9661b5483f4
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-05 02:21:54 +0100

    Update DCNN-DANN.py

--- DCNN-DANN.py
+++ DCNN-DANN.py
@@ -195,13 +195,12 @@ def build_bilstm(word_index, embeddings_dict, optimizer='adam', MAX_SEQUENCE_LEN
   
     model2=Conv1D(128, 5, activation="relu")(embedding_layer)
     model2= MaxPooling1D(2)(model2)
-    model2 = Dense(units=128, activation='relu')(model2)
-    model2 = Dense(units=64, activation='relu')(model2)
+    model2 = Lstm(128,return_sequences=True)(model2)
+    model2 = Lstm(64)(model2)
     model2 = Dropout(rate=0.5)(model2)
-    model2 = Dense(units=64, activation='relu')(model2)
     model2= Dense(256,activation='relu')(model2)
     
-    #model2 = Dropout(rate=0.5)(model2)
+   
     model3 = layers.concatenate([model1,model2])
     model3 = Dense(512, activation='relu')(model3)
     #model3 = Dropout(0.5)(model3)
