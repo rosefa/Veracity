6055c0a8b0a403eed116b02ab392fad84319bfbd
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-07 16:19:42 +0100

    Update vdc-vdlstm.py

--- vdc-vdlstm.py
+++ vdc-vdlstm.py
@@ -204,7 +204,8 @@ def prepare_model_input(X_train, X_test,MAX_NB_WORDS=75000,MAX_SEQUENCE_LENGTH=3
     #return (text, word_index, embeddings_dict)
 
 def build_bilstm(word_index, embeddings_dict, MAX_SEQUENCE_LENGTH=300, EMBEDDING_DIM=100):
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.3)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+    #optimizer = tf.keras.optimizers.Adam(learning_rate=0.3)
     input = Input(shape=(300,), dtype='int32')
     embedding_matrix = np.random.random((len(word_index)+1, 100))
     for word, i in word_index.items():
