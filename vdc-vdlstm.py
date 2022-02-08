5c3e288114be88f28d4ff38d319e178e1bd81e0f
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-08 12:52:29 +0100

    Update vdc-vdlstm.py

--- vdc-vdlstm.py
+++ vdc-vdlstm.py
@@ -14,7 +14,7 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
-from keras.layers import GlobalAveragePooling1D,GlobalMaxPooling1D,Flatten,Concatenate, Dropout, LSTM, Bidirectional,Conv1D,MaxPooling1D,Flatten,BatchNormalization
+from keras.layers import GlobalAveragePooling1D,GlobalMaxPooling1D,Flatten,Concatenate, Dropout, LSTM, Bidirectional,Conv1D,MaxPooling1D,Flatten,BatchNormalization,Activation
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
