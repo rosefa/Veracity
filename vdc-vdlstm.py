735508925fb4d4268d26cae52c3679af95669e8a
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-08 11:37:08 +0100

    Update vdc-vdlstm.py

--- vdc-vdlstm.py
+++ vdc-vdlstm.py
@@ -216,21 +216,24 @@ def build_bilstm(word_index, embeddings_dict, MAX_SEQUENCE_LENGTH=300, EMBEDDING
 
    
     
-    model1=Conv1D(128, 5,activation="relu")(embedding_layer)
+    model1=Conv1D(64, 3,activation="relu")(embedding_layer)
     #model1 = BatchNormalization()(model1)
     #model1 =MaxPooling1D(2)(model1)
-    model1= Conv1D(128, 5,activation="relu")(model1)
+    model1= Conv1D(64, 64,activation="relu")(model1)
     model1 =MaxPooling1D(2)(model1)
     #model1 = BatchNormalization()(model1)
     #model1 =MaxPooling1D(2)(model1)
-    model1= Conv1D(128,5,activation='relu')(model1)
-    model1= Conv1D(128,5,activation='relu')(model1)
+    model1= Conv1D(128,128,activation='relu')(model1)
+    model1= Conv1D(256,256,activation='relu')(model1)
     #model1 = BatchNormalization()(model1)
-    #model1 =MaxPooling1D(2)(model1)
-    model1= GlobalMaxPooling1D()(model1)
-    model1 = Bidirectional(LSTM(64,recurrent_dropout=0.2))(model1)
-    #model1 = Dense(128, activation='relu')(model1)
-    model1 = Dropout(0.5)(model1)
+    model1 =MaxPooling1D(2)(model1)
+    #model1= GlobalMaxPooling1D()(model1)
+    model1 = LSTM(128)(model1)
+    #model1 = Bidirectional(LSTM(64,recurrent_dropout=0.2))(model1)
+    model1 = Dense(256, activation='relu')(model1)
+    model1 = Dense(256, activation='relu')(model1)
+    model1 = Dense(256, activation='relu')(model1)
+    #model1 = Dropout(0.5)(model1)
     model1 = Dense(1,activation='sigmoid')(model1)
     
     
