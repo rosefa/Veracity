4c1e1792ca79c2c9f8534c0574cdd06944c665e6
rosefa <82177268+rosefa@users.noreply.github.com>
2022-02-08 16:05:21 +0100

    Update vdc_bilstm.py

--- vdc_bilstm.py
+++ vdc_bilstm.py
@@ -23,7 +23,7 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
-from keras.layers import Dropout, Dense, Embedding, LSTM, Bidirectional,Conv1D,MaxPooling1D,Flatten
+from keras.layers import Dropout, Dense, Embedding, LSTM, Bidirectional,Conv1D,MaxPooling1D,Flatten,Input
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
